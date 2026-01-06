978ffde2636c485a0e09ebda9249e352b5f93f1c
Hitesh Chauhan <31502924+hiteshchauhan89@users.noreply.github.com>
2026-01-06 17:20:30 +0530

    Add files via upload

--- Terraform Advance/conditional statement/create_instance.tf
+++ Terraform Advance/conditional statement/create_instance.tf
@@ -1,10 +1,15 @@
+provider "aws" {
+  region = var.AWS_REGION
+}
+
 module "ec2-cluster" {
-    source = " github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
+    source  = "terraform-aws-modules/ec2-instance/aws"
 
     name = "my-cluster"
     ami = "ami-0ecb62995f68bb549"
     instance_type= "t2.micro"
     subnet_id ="subnet-0ca16fdf866acfc79"
+    count = var.environment == "Production" ? 2 : 1
 
     tags={
     Terraform = "true"
---
+++ TerraformModules/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = "AKIARQTU3FTHXSYXDNS6"
+  secret_key = ""
+  region     = "us-east-1"
+}
